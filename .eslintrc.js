377f6c843b28cdec7e9c8b349325333676484267
Matthew Soul <de-soul@inbox.ru>
2021-03-19 18:40:48 +0300

    Airbnb code styling support added to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-  "env": {
-  	"browser": true,
-    "commonjs": true,
-    "es6": true,
-    "node": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
   },
-  "extends": [ "prettier", "eslint:recommended"],
-  "plugins": [ "prettier"],
-  "parser": "@babel/eslint-parser",
-  "rules": {
-    "max-len": [2, {"code": 140, "tabWidth": 2, "ignoreUrls": true}],
+  extends: ["airbnb-base", "prettier", "eslint:recommended"],
+  plugins: ["prettier"],
+  parser: "@babel/eslint-parser",
+  rules: {
+    "max-len": [2, { code: 140, tabWidth: 2, ignoreUrls: true }],
     "no-console": "off",
     "no-underscore-dangle": "off",
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
   },
-  "settings": {
+  settings: {
     "import/resolver": {
-      "node": {
-        "paths": ["src"],
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
-  }
-}
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
