33aa26fd5c0395f638b0b0374f265f7fba73ef81
Matthew Soul <de-soul@inbox.ru>
2021-03-21 15:58:29 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ["airbnb-base", "prettier", "eslint:recommended"],
-  plugins: ["prettier"],
+  extends: ["eslint-config-airbnb-base", "eslint-config-prettier"],
+  plugins: ["eslint-plugin-prettier"],
   parser: "@babel/eslint-parser",
   rules: {
     "max-len": [2, { code: 140, tabWidth: 2, ignoreUrls: true }],
